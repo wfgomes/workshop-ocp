ae100651478d51c9c48c03ee9f6eca4833734058
Wesckley Gomes <wesckley.gomes@gmail.com>
2018-04-12 18:29:52 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1>Openshift Workshop v1.0</h1> ";
+echo "<h1>Openshift Workshop v2.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
-echo "Testando Webhook";
+echo "<br><h3>Testando Webhook</h3>";
 ?>
